1a19494699948dd2ffa74c371d3792d098ba2a68
Martin Lang <m.lang@soton.ac.uk>
2020-11-25 16:39:11 +0100

    Small sample project with one function.

---
+++ sampleproject/function.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    """Adds two numbers.
+
+    Function to add two numbers.
+
+    Parameters
+    ----------
+    a
+
+        First number.
+
+    b
+
+        Second number.
+
+    Returns
+    -------
+        Sum of a and b.
+    """
+    return a + b
