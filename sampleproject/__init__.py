fd4b56136734d6cbeb04a908d4ff79d312d60224
marijanbeg <m.beg@soton.ac.uk>
2020-12-02 14:16:28 +0000

    Further work.

--- sampleproject/__init__.py
+++ sampleproject/__init__.py
@@ -1,1 +1,10 @@
+import pytest
+import pkg_resources
 from .function import add
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
+
+
+def test():
+    return pytest.main(['-v', '--pyargs', 'sampleproject'])  # pragma: no cover
