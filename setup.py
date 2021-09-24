2a4fd6c50b4e884fc3e449c9b77e6fe4be8dbe3d
Martin Lang <m.lang@soton.ac.uk>
2021-09-24 10:53:15 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='blaupause',
-    version='0.1.2',
+    version='0.1.3',
     description=('Example package used as a template for setting up '
                  'Python packages/repositories.'),
     # these two lines allow using markdown README
